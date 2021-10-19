f34ee534e7613ed839b9416056532eb64e719924
Lucas Stefano <lucassxs.sp@gmail.com>
2021-10-19 13:10:22 -0300

    📝fix function ketnipz

--- R/ketnipz_r.R
+++ R/ketnipz_r.R
@@ -5,4 +5,15 @@
 #' @export
 #'
 
-ketnipz_r <- function()
+ketnipz_r <- function(){
+  tmp <- tempfile(fileext = ".jpeg")
+  utils::download.file(
+    "https://www.instagram.com/ketnipz/",
+    tmp,
+    quiet = TRUE
+  )
+  tmp %>%
+    jpeg::readJPEG() %>%
+    grDevices::as.raster() %>%
+    graphics::plot()
+}
