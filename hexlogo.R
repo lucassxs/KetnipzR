a8719a28abad516836789dff7ca87b189c8afb9d
Lucas Stefano <lucassxs.sp@gmail.com>
2022-01-09 12:15:08 -0300

    🎨change logo color

--- hexlogo.R
+++ hexlogo.R
@@ -1,6 +1,11 @@
 library(hexSticker)
+library(sysfonts)
+library(showtextdb)
+library(showtext) ## Loading Google fonts (http://www.google.com/fonts)
+font_add_google("Fira Sans", "sans-serif") ## Source font: https://fonts.google.com/specimen/Fira+Sans?query=fira#standard-styles
+showtext_auto()
 
 imgPath <- "avatar.png"
 
 sticker(imgPath, package = "KetnipzR", p_size=20, s_x=1, s_y=.75, s_width=.6,
-        filename = "hexketnipz.png")
+        h_fill="#ffbbba",h_color="#d5633f", filename = "hexketnipz.png")
